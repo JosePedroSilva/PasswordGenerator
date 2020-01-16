acf48d35ae7e37ee3174eaf476f56b78cbcaaaca
JosePedro <josepedro88@live.com.pt>
2020-01-16 14:20:11 +0000

    Clear button and label, button rearrangement

--- passGen.py
+++ passGen.py
@@ -5,18 +5,20 @@ import tkinter as tk
 from string import ascii_letters, digits
 from random import choice
 
-SYMBOL = '!?'
-passw = ''.join([choice(ascii_letters + digits + SYMBOL)
-                 for i in range(10)])
-
 
 def generate():
+    SYMBOL = '!?'
+    passw = ''.join([choice(ascii_letters + digits + SYMBOL)
+                     for i in range(10)])
     entry.delete(0, tk.END)
     entry.insert(0, passw)
 
 
+def clear():
+    entry.delete(0, tk.END)
+
+
 root = tk.Tk()
-root.geometry('400x200')
 
 
 label = tk.Label(root, text="Password Generator")
@@ -25,9 +27,11 @@ label.pack(padx=10, pady=10)
 entryText = tk.StringVar()
 entry = tk.Entry(root, textvariable=entryText)
 entryText.set(generate)
-entry.pack(padx=10, pady=10)
+entry.pack(side=tk.TOP)
 
 button = tk.Button(root, text='Generate', command=generate)
-button.pack()
+button2 = tk.Button(root, text='Clear', command=clear)
+button.pack(side=tk.LEFT)
+button2.pack(side=tk.LEFT)
 
 root.mainloop()
