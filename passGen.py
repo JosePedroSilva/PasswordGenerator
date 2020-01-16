7262029d3bd10d43af89a316838c42e108105650
JosePedro <josepedro88@live.com.pt>
2020-01-16 13:40:03 +0000

    Player choice done and password printed

--- passGen.py
+++ passGen.py
@@ -4,5 +4,13 @@
 from string import ascii_letters, digits
 from random import choice
 
+SYMBOL = '!?'
 
-passw = ''.join([choice(ascii_letters + digits) for i in range(32)])
+userInput = input('Which size do you want the password to have?\n>>> ')
+userInput = int(userInput)
+
+passw = ''.join([choice(ascii_letters + digits + SYMBOL)
+                 for i in range(userInput)])
+
+print('Your new pass is:\n')
+print(passw)
