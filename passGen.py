67236cfdd3c36ee4933f24de64ad99dd1483efdd
JosePedro <josepedro88@live.com.pt>
2020-01-16 14:12:27 +0000

    Button to generate created

--- passGen.py
+++ passGen.py
@@ -5,20 +5,29 @@ import tkinter as tk
 from string import ascii_letters, digits
 from random import choice
 
+SYMBOL = '!?'
+passw = ''.join([choice(ascii_letters + digits + SYMBOL)
+                 for i in range(10)])
+
+
+def generate():
+    entry.delete(0, tk.END)
+    entry.insert(0, passw)
+
+
 root = tk.Tk()
 root.geometry('400x200')
 
-SYMBOL = '!?'
-
-passw = ''.join([choice(ascii_letters + digits + SYMBOL)
-                 for i in range(10)])
 
 label = tk.Label(root, text="Password Generator")
 label.pack(padx=10, pady=10)
 
 entryText = tk.StringVar()
 entry = tk.Entry(root, textvariable=entryText)
-entryText.set(passw)
+entryText.set(generate)
 entry.pack(padx=10, pady=10)
+
+button = tk.Button(root, text='Generate', command=generate)
+button.pack()
 
 root.mainloop()
